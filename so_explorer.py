f66654a168d910587d6bf646ce781e95655432fe
Aris Adamantiadis <aris@0xbadc0de.be>
2020-08-14 18:02:05 +0200

    hash files

--- so_explorer.py
+++ so_explorer.py
@@ -2,6 +2,7 @@
 
 import sys
 from subprocess import Popen, PIPE, DEVNULL
+from Crypto.Hash import SHA256
 
 def read_symbols(filename):
 	p = Popen(["/usr/bin/nm", "-D", "-C", filename], stdin=DEVNULL, stdout=PIPE, bufsize=2048)
@@ -20,10 +21,19 @@ def read_symbols(filename):
 		symbols.append(v)
 	return symbols
 
-
+def hash_file(filename):
+	h = SHA256.new()
+	with open(filename, "rb") as f:
+		while True:
+			buf = f.read(4096)
+			if len(buf) == 0:
+				break
+			h.update(buf)
+	return h.hexdigest()
 
 def main(args):
 	read_symbols(args[1])
+	print(hash_file(args[1]))
 
 if __name__== "__main__":
 	main(sys.argv)
